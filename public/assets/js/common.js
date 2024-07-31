73be6b1474f76361b94a59c731dc8de5eb5e36e8
Rikiya Okawa <83910358+Rikiya89@users.noreply.github.com>
2024-07-31 14:46:21 +0900

    20240730

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,12 +1,53 @@
 const modal = document.querySelector('.js-modal'); // layer要素に付与したjs-modalクラスを取得し変数に格納
 const modalButton = document.querySelector('.js-modal-button'); // button要素に付与したjs-modal-buttonクラスを取得し、変数に格納
-const modalClose = document.querySelector('.js-close-button');// xボタンのjs-close-buttonを取得し変数に格納
+const modalClose = document.querySelector('.js-close-button'); // xボタンのjs-close-buttonを取得し変数に格納
 
 // モーダルボタンをクリックしたときのイベントを登録
-modalButton.addEventListener('click', () => {
-    modal.classList.add('is-open');
-});
+if (modalButton) {
+    modalButton.addEventListener('click', () => {
+        if (modal) {
+            modal.classList.add('is-open');
+        }
+    });
+}
 
-modalClose.addEventListener('click', () => { // xボタンをクリックしたときのイベントを登録
-    modal.classList.remove('is-open'); 
-});
+if (modalClose) {
+    modalClose.addEventListener('click', () => { // xボタンをクリックしたときのイベントを登録
+        if (modal) {
+            modal.classList.remove('is-open');
+        }
+    });
+}
+
+function activateAccordion() {
+    const accordions = document.querySelectorAll(".accordion-toggle");
+    const isSmallScreen = window.innerWidth <= 768;
+
+    accordions.forEach(function(accordion) {
+        const content = accordion.nextElementSibling;
+        const icon = accordion.querySelector('.accordion-icon');
+
+        if (isSmallScreen) {
+            accordion.addEventListener("click", function() {
+                // Toggle the content
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                    icon.classList.remove('open');
+                } else {
+                    content.style.display = "block";
+                    icon.classList.add('open');
+                }
+            });
+        } else {
+            content.style.display = "block"; // Ensure content is displayed on larger screens
+            icon.classList.remove('open'); // Reset icon on larger screens
+            accordion.removeEventListener("click", function() {});
+        }
+    });
+}
+
+// Run on initial load
+activateAccordion();
+
+// Run on resize
+window.addEventListener('resize', activateAccordion);
